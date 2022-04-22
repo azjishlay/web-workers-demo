86fd0c4725eb0426cd322db03e3a79b6b6ccc133
azjishlay <16637558+azjishlay@users.noreply.github.com>
2022-04-22 01:42:54 -0400

    Updates quiz solution

--- scripts/image-app.js
+++ scripts/image-app.js
@@ -43,20 +43,47 @@
     // Hint! This is where you should post messages to the web worker and
     // receive messages from the web worker.
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
+    if (window.Worker) {
+      const imageWorker = new Worker("scripts/worker.js");
+
+      imageWorker.postMessage({"imageData": imageData, "type": type});
+      console.log('Message posted to worker');
+
+      imageWorker.onmessage = function (e) {
+        toggleButtonsAbledness();
+        imageData = e.data;
+        console.log('Message received from worker');
+
+        if (imageData) return ctx.putImageData(imageData, 0, 0);
+        console.log('No image');
+      }
+
+      imageWorker.onerror = function(error) {
+        function WorkerException(message) {
+          this.name = "WorkerException";
+          this.message = message;
+        }
+        throw new WorkerException('Worker error');
+      }
+    } else {
+      console.log('Your browser doesn\'t support web workers.');
+
+      length = imageData.data.length / 4;
+      for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+        r = imageData.data[i * 4 + 0];
+        g = imageData.data[i * 4 + 1];
+        b = imageData.data[i * 4 + 2];
+        a = imageData.data[i * 4 + 3];
+        pixel = manipulate(type, r, g, b, a);
+        imageData.data[i * 4 + 0] = pixel[0];
+        imageData.data[i * 4 + 1] = pixel[1];
+        imageData.data[i * 4 + 2] = pixel[2];
+        imageData.data[i * 4 + 3] = pixel[3];
+      }
+
+      toggleButtonsAbledness();
+      return ctx.putImageData(imageData, 0, 0);
     }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
   };
 
   function revertImage() {
